f7f943f1b330e0af19ae6d11bc7847c844d82815
Draig9 <jacjoneslfc@gmail.com>
2017-10-25 17:56:07 +0100

    renamed file,variables and fixed errors
    found some of the errors in the code, changed the main file name so arduino ide was happy, renamed some variable names to be more consistent

---
+++ hinge-bot.ino
@@ -0,0 +1,116 @@
+//globals
+
+//direction and speeds
+byte left_direction = 13;
+byte right_direction = 12;
+byte left_speed = 11;
+byte right_speed = 3;
+//sonar globals
+int trigger = 4;
+int echo = 2;
+//distance state (used for wall checks)
+int orig_state = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+
+  //directions
+  pinMode(left_direction, OUTPUT);
+  pinMode(right_direction, OUTPUT);
+
+  //sonar
+  Serial.begin(9600);
+  pinMode(trigger, OUTPUT);
+  pinMode(echo, INPUT);
+}
+
+void robot_reverse(){
+  digitalWrite(left_direction, HIGH);
+  analogWrite(left_speed, 255);
+  digitalWrite(right_direction, HIGH);
+  analogWrite(right_speed, 255);
+}
+
+void robot_forwards(){
+  digitalWrite(left_direction, LOW);
+  analogWrite(left_speed, 255);
+  digitalWrite(right_direction, LOW);
+  analogWrite(right_speed, 255);
+}
+
+void robot_left(){
+  digitalWrite(left_direction, LOW);
+  analogWrite(left_speed, 255);
+  digitalWrite(right_direction, HIGH);
+  analogWrite(right_speed, 255);
+}
+
+void robot_right(){
+  digitalWrite(left_direction, HIGH);
+  analogWrite(left_speed, 255);
+  digitalWrite(right_direction, LOW);
+  analogWrite(right_speed, 255);
+}
+
+void robot_stop(){
+  digitalWrite(left_direction, LOW);
+  analogWrite(left_speed, 0);
+  digitalWrite(right_direction, LOW);
+  analogWrite(right_speed, 0);
+}
+void print_sonar_debug(float duration){
+  Serial.print("duration : ");
+  Serial.println(duration);
+  Serial.print("distance : ");
+  Serial.println(sonar_to_distance(duration));
+}
+
+float sonar_duration(){
+  long duration;
+  digitalWrite(trigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigger, LOW);
+  duration = pulseIn(echo, HIGH);
+  //if debugging uncomment 
+  //print_sonar_debug(duration);
+  return duration;
+}
+float sonar_to_distance(float t){
+ float speed_of_sound = 343;
+ return (((t/2) * speed_of_sound) / 1000);
+}
+float sonar_distance(){
+  return sonar_to_distance(sonar_duration());
+}
+
+enum distance_states{
+  FORWARD_CLEAR,
+  FORWARD_BLOCKED,
+  RIGHT_BLOCKED,
+  LEFT_BLOCKED
+};
+void nav_maze(){
+  switch(orig_state){
+    case 0:
+    robot_forwards();
+    if(sonar_distance() > 100){
+      robot_stop();
+      robot_left();
+      
+    }
+  }
+}
+
+void robot_right_degrees(float deg){
+  
+}
+void robot_left_degrees(float deg){
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  nav_maze();
+}
+
+
